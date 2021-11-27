6635a69e336277853825e0d300934ab834ef11d2
DPlanells <93334946+DPlanells@users.noreply.github.com>
2021-11-27 17:24:52 +0100

    Update nivelC.c

--- Entrega 2/nivelC.c
+++ Entrega 2/nivelC.c
@@ -59,6 +59,10 @@ void ctrlc(int signum);
 void ctrlz(int signum);
 char *comando;
 
+int jobs_list_add(pid_t pid, char status, char *cmd);
+int jobs_list_find(pid_t pid);
+int jobs_list_remove(int pos);
+
 int n_pids = 0;
 
 static char mi_shell[COMMAND_LINE_SIZE]; //variable global para guardar el nombre del minishell
@@ -108,7 +112,9 @@ int internal_source(char **args) {
 
 int internal_jobs(char **args) {
     #if DEBUGN1 
-        printf("[internal_jobs()→ Esta función mostrará el PID de los procesos que no estén en foreground]\n");
+        for (size_t i = 1; i <= n_pids; i++){
+        printf("Trabajo %d)    PID: %d   Comando: %s   Estatus: %s"  i, jobs_list[i].pid, jobs_list[i].cmd, jobs_list[i].status);
+    }
     #endif
     return 1;
 }
@@ -345,6 +351,41 @@ void ctrlz(int signum)
     }
 
     
+}
+
+
+int jobs_list_add(pid_t pid, char status, char *cmd){
+
+    if (n_pids<N_JOBS){
+        jobs_list[n_pids].pid=pid;
+        jobs_list[n_pids].status=status;
+        strcpy(jobs_list[n_pids].cmd,cmd);
+        n_pids++;
+        return 0;
+    }
+    
+    return -1;
+}
+
+int jobs_list_find(pid_t pid){
+
+    for (int pos = 0; pos < n_pids; pos++){
+        if (jobs_list[pos].pid==pid){
+            return pos;
+        }
+    }
+
+    return -1;
+}
+
+
+int  jobs_list_remove(int pos){
+
+    jobs_list[pos].pid=jobs_list[n_pids].pid;
+    jobs_list[pos].status=jobs_list[n_pids].status;
+    strcpy(jobs_list[pos].cmd,jobs_list[n_pids].cmd);
+    n_pids--;
+    return 0;
 }
 
 
